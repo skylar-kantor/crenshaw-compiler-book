8d8ae6309ae47325d6bd1a30626f34d9b06e9393
skantor-mtb <139501569+skantor-mtb@users.noreply.github.com>
2024-01-11 20:59:44 +0000

    Chapter 2

--- cradle.c
+++ cradle.c
@@ -4,14 +4,13 @@
 #include <string.h>
 char look = '\0';
 
-
-void getch(void) 
+void getch(void)
 {
     look = getchar();
 }
 void error(char *s)
 {
-    printf("\n%s.", s);
+    printf("\n%s\n", s);
 }
 
 void fatal(char *s)
@@ -22,7 +21,7 @@ void fatal(char *s)
 
 void expected(char *s)
 {
-    char *out = malloc((strlen(s)+1)*sizeof(char));
+    char *out = malloc((strlen(s) + 1) * sizeof(char));
     sprintf(out, "%s expected", s);
     fatal(out);
 }
@@ -39,7 +38,7 @@ void match(char x)
     }
 }
 
-char get_name(void) 
+char get_name(void)
 {
     if (!isalpha(look))
     {
@@ -50,7 +49,7 @@ char get_name(void)
     return new_name;
 }
 
-char get_num(void) 
+char get_num(void)
 {
     if (!isdigit(look))
     {
@@ -78,8 +77,92 @@ void init(void)
     getch();
 }
 
+void factor(void)
+{
+    char result[100];
+    sprintf(result, "mov ebx, %c", get_num());
+    emitln(result);
+}
+
+void multiply(void)
+{
+    match('*');
+    factor();
+    emitln("pop ecx");
+    emitln("mul ebx, ecx");
+}
+
+void divide(void)
+{
+    match('/');
+    factor();
+    emitln("pop ecx");
+    emitln("div ecx, ebx");
+}
+
+void term(void)
+{
+    factor();
+    while (look == '*' || look == '/')
+    {
+        emitln("push ebx");
+        if (look == '*')
+        {
+            multiply();
+        }
+        else if (look == '/')
+        {
+            divide();
+        }
+        else
+        {
+            expected("Mulop");
+        }
+        
+    }
+}
+
+void add()
+{
+    match('+');
+    term();
+    emitln("pop ecx");
+    emitln("add ebx, ecx");
+}
+
+void subtract()
+{
+    match('-');
+    term();
+    emitln("pop ecx");
+    emitln("sub ebx, ecx");
+    emitln("neg ebx");
+}
+
+void expression(void)
+{
+    term();
+    while (look == '+' || look == '-')
+    {
+        emitln("push ebx");
+        if (look == '+')
+        {
+            add();
+        }
+        else if (look == '-')
+        {
+            subtract();
+        }
+        else
+        {
+            expected("Addop");
+        }
+    }
+}
+
 int main()
 {
     init();
+    expression();
     return 0;
 }
